55c91a380d2937da82f7e342f9134f352eb9ff09
Jakub Brych <brychjakub@gmail.com>
2023-06-25 15:48:32 +0200

    fighting the edit button

--- phpProject/events/event_list.php
+++ phpProject/events/event_list.php
@@ -35,23 +35,24 @@ $pdo = null;
     <meta charset="UTF-8">
     <title>Rezervace CMcZŠ</title>
     <link rel="stylesheet" href="../styles.css">
-    <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body class="container">
+    <div  class="sidebar">
+
     <ul>
         <li><a href="create_event.html">Vytvořit událost</a></li>
         <li><a href="event_list.php">Události</a></li>
         <li><a href="../questions/questions.html">Dotazník</a></li>
     </ul>
+    </div>
     <section>
         <h2>Seznam událostí</h2>
         <table>
             <thead>
                 <tr>
-                    <th id="event-name">Událost</th>
-                    <th id="event-date">Datum</th>
-                    <th id="event-link">Odkaz</th>
-                    <th id="event-edit">Editace</th>
+                    <th id="event-name">Název</th>
+                    <th id="event-date">Vytvořeno</th>
+                    <th id="event-edit">Úprava</th>
                     <th id="event-state">Stav</th>
                     <th id="event-delete">Smazat</th>
                 </tr>
@@ -61,14 +62,10 @@ $pdo = null;
                     <tr>
                         <td headers="event-name"><a href="/admin/event/<?php echo $event['id']; ?>/overview/view"><?php echo $event['eventName']; ?></a></td>
                         <td headers="event-date"><?php echo $event['startDate']; ?> - <?php echo $event['endDate']; ?></td>
-                        <td headers="event-link">
-                            <a href="/user/event/<?php echo $event['id']; ?>/view">
-                                <span class="icon-link"></span>
-                            </a>
-                        </td>
+                        
                         <td headers="event-edit">
-                            <a href="/admin/event/edit/<?php echo $event['id']; ?>/view">
-                                <span class="icon-edit"></span>
+                            <a href="edit_event.php?edit=<?php echo $event['id']; ?>/view">
+                                <span class="icon-edit">✏️</span>
                             </a>
                         </td>
                         <td headers="event-state">
@@ -76,7 +73,7 @@ $pdo = null;
                         </td>
                         <td headers="event-delete">
                             <a href="delete_event.php?delete=<?php echo $event['id']; ?>" onclick="return confirm('Are you sure you want to delete this event?');">
-                                <span class="icon-delete"></span>
+                                <span class="icon-delete">🗑️</span>
                             </a>
                         </td>
                     </tr>
